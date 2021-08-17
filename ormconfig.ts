7e987bb8cdab9bcd0c4170c58af682d3a0dc47c2
SolChanP <qkrthfcks1@naver.com>
2021-08-18 07:03:55 +0900

    TypeORM Setting and dotenv confingure

---
+++ ormconfig.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Schedule } from './src/entities/schedule';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.HOST,
+  port: 5432,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  entities: [Schedule],
+  synchronize: false,
+};
+
+module.exports = config;
